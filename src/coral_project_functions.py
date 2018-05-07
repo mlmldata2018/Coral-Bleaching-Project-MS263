677e582fa449428e5627f7524d406b620c1cd0ee
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-05-07 15:13:40 -0700

    Adding total bleaching plots
    Note that the external plot function, bleach_annual_plot is not working yet.

--- src/coral_project_functions.py
+++ src/coral_project_functions.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 from scipy import stats
+import numpy as np
 
 
 def make_coral_map(projection=ccrs.Miller(central_longitude=180),
@@ -83,3 +84,62 @@ def bleach_scatter(df, set_name):
     plt.plot(df['Severe count'], intercept + slope*df['Severe count'],
              'r', label='fitted line')
     print("r-squared:", r_value**2)
+
+def bleach_annual_plot(branching_bleach, massive_bleach, hughes,
+                       c_idx=None, h_idx=None):
+    # The annual bleaching count for the whole world, based on Logan et al. (2018)
+
+
+    if c_idx is None:
+        # c_idx = np.ones(len(branching_bleach[:, 0]), dtype=int)
+        bb = branching_bleach
+        mb = massive_bleach
+    else:
+        bb = branching_bleach[c_idx]
+        mb = massive_bleach[c_idx]
+    if h_idx is None:
+        hb = hughes
+    else:
+        hb = hughes.iloc(h_idx)
+
+    cell_annual = np.sum(mb | bb, 0)
+
+    # Hughes severe events are flagged by the letter S in columns called 1980, 1981, etc.
+    hughes_annual = np.zeros(2016-1980+1)
+    for y in range(1980, 2017):
+        hughes_annual[y-1980] = sum(hb[y] == 'S')
+
+    hughes_annual = np.int32(hughes_annual)
+
+    hughes_norm = np.mean(hughes_annual)*hughes_annual/sum(hughes_annual)
+    cell_norm = np.mean(hughes_annual)*cell_annual/sum(cell_annual)
+
+    # Plot global cumulative total by year for both approaches.
+    plt.figure()
+    plt.plot(range(1980, 2017), hughes_norm, label='Hughes')
+    plt.plot(range(1980, 2017), cell_norm, label='Logan')
+    plt.xlabel('Year')
+    plt.ylabel('Normalized bleaching count')
+    plt.legend()
+    plt.figure()
+
+    shiftYear = -1.5
+    plt.plot(range(1980, 2017), hughes_norm, label='Hughes')
+    plt.plot(shiftYear+np.array(range(1980, 2017)), cell_norm, label='Logan')
+    plt.legend()
+
+
+if __name__ == '__main__':
+    import pandas as pd
+
+    print('Begin standalone diagnostics')
+    df = pd.DataFrame(np.random.randint(0, high=10, size=(100, 37)),
+        columns=range(1980, 2017))
+
+
+    number to S
+
+
+    bleach_annual_plot(np.random.randint(0, high=10, size=(1925, 37)),
+                       np.random.randint(0, high=10, size=(1925, 37)),
+                       df)
